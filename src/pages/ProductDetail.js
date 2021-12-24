e1c95c1a04cfc0bd077cf58e6d85a6c82416ab97
Augusto Forero <a.forarc@gmail.com>
2021-12-24 09:06:29 -0500

    Create ProductDetail page and design SingleProduct

---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,14 @@
+// Material UI
+import { Container } from '@mui/material';
+// Components
+import SingleProduct from '../components/SingleProduct';
+
+const ProductDetail = () => {
+  return (
+    <Container>
+      <SingleProduct />
+    </Container>
+  );
+};
+
+export default ProductDetail;
