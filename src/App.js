8d66f99ec8b7b5027770f5a90c61413cbcc844ab
Augusto Forero <a.forarc@gmail.com>
2021-12-24 08:59:49 -0500

    Add React Router and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import * as React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// Material UI
+import { Toolbar } from '@mui/material';
+// Components
+import NavBar from './components/NavBar';
+// Pages
+import About from './pages/About';
 import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
 
 const App = () => {
-  return <Home />;
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Toolbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<ProductDetail />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
